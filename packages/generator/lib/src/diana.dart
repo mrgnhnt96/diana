dde44cc0c44968968c004ce80ba6e28d86934fe8
Morgan Hunt <mrgnhnt96@gmail.com>
2021-11-27 11:04:34 -0600

    feat: start generator

--- packages/generator/lib/src/diana.dart
+++ packages/generator/lib/src/diana.dart
@@ -1,7 +1,21 @@
-/// {@template diana}
-/// A Very Good Project created by Very Good CLI.
+import 'package:analyzer/dart/element/element.dart';
+import 'package:build/build.dart';
+import 'package:diana_annotation/diana.dart';
+import 'package:source_gen/source_gen.dart';
+
+/// {@template diana_generator}
+/// A [Generator] that generates a [Diana] class for a given [ClassElement].
 /// {@endtemplate}
-class Diana {
-  /// {@macro diana}
-  const Diana();
+class DianaGenerator extends GeneratorForAnnotation<Diana> {
+  /// {@macro diana_generator}
+  const DianaGenerator();
+
+  @override
+  Iterable<String> generateForAnnotatedElement(
+    Element element,
+    ConstantReader annotation,
+    BuildStep buildStep,
+  ) {
+    return {};
+  }
 }
